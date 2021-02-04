013d01192225deab6d79e281da54dce21fda26d3
Sybille Peters <sypets@gmx.de>
2021-02-04 15:32:14 +0100

    [TASK] Update description

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Page Messages',
-    'description' => 'Add flash messages to page module in TYPO3 backend',
+    'description' => 'Add callouts to page module in TYPO3 backend',
     'category' => 'module',
     'author' => 'Sybille Peters',
     'author_email' => 'sypets@gmx.de',
@@ -10,7 +10,7 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
