1b3037d66533abb77258830fe4231f08ed615218
Sybille Peters <sypets@gmx.de>
2023-06-12 07:08:59 +0200

    [TASK] Remove usage of TYPO3_MODE
    This is deprecatd and should be removed.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,10 +1,10 @@
 <?php
 
-defined('TYPO3_MODE') or die();
+use Sypets\PageCallouts\Xclass\PageLayoutControllerWithCallouts;
+use TYPO3\CMS\Backend\Controller\PageLayoutController;
 
-// encapsulate all locally defined variables
-(function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\TYPO3\CMS\Backend\Controller\PageLayoutController::class] = [
-        'className' => \Sypets\PageCallouts\Xclass\PageLayoutControllerWithCallouts::class
-    ];
-})();
+defined('TYPO3') or die();
+
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][PageLayoutController::class] = [
+    'className' => PageLayoutControllerWithCallouts::class
+];
