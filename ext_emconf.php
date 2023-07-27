135acca849bf036db42b4d1f9f10daefed5518f6
Sybille Peters <sypets@gmx.de>
2023-07-27 16:13:24 +0200

    v3.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,12 +11,12 @@ $EM_CONF[$_EXTKEY] = [
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '3.0.1',
+    'version' => '3.0.2',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.27-11.5.99',
-            'backend' => '11.5.27-11.5.99',
-            'fluid' => '11.5.27-11.5.99',
+            'typo3' => '11.5.30-11.5.99',
+            'backend' => '11.5.30-11.5.99',
+            'fluid' => '11.5.30-11.5.99',
         ],
         'conflicts' => [
             'qc_be_pagelanguage' => '1.0.0-9.9.99'
