7e98f977ee0065703e6f7a1ba6603f49dd8c5390
Sybille Peters <sypets@gmx.de>
2024-02-05 18:04:02 +0100

    Drop support for TYPO3 v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,9 +11,9 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '4.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.30-12.4.99',
-            'backend' => '11.5.30-12.4.99',
-            'fluid' => '11.5.30-12.4.99',
+            'typo3' => '12.4.10-12.4.99',
+            'backend' => '12.4.10-12.4.99',
+            'fluid' => '12.4.10-12.4.99',
         ],
         'conflicts' => [
             'qc_be_pagelanguage' => '1.0.0-9.9.99'
